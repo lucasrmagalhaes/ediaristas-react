3cbcc16eb3f3020f3bf9ee4bec1bc27535190d7b
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-06-10 09:47:51 -0300

    UserInformation

--- ediaristas/src/pages/index.tsx
+++ ediaristas/src/pages/index.tsx
@@ -1,6 +1,6 @@
-import Head from "next/head";
 import SafeEnvironment from "ui/components/feedback/SafeEnvironment/SafeEnvironment";
 import PageTitle from "ui/components/data-display/PageTitle/PageTitle";
+import UserInformation from "ui/components/data-display/UserInformation/UserInformation";
 
 export default function Home() {
   return (
@@ -11,6 +11,12 @@ export default function Home() {
         subtitle={
           "Preencha seu endereço e veja todos os profissionais da sua localidade"
         }
+      />
+      <UserInformation
+        name={"Lucas"}
+        picture={"https://github.com/lucasrmagalhaes.png"}
+        rating={3}
+        description={"Canoas"}
       />
     </div>
   );
