df27e931850f041ca91966118a89ab5e7a281af4
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-06-12 17:52:33 -0300

    Footer finalizado

--- ediaristas/src/pages/_app.tsx
+++ ediaristas/src/pages/_app.tsx
@@ -3,6 +3,8 @@ import { ThemeProvider } from "@material-ui/core";
 import theme from "ui/themes/theme";
 import Head from "next/head";
 import Header from "ui/components/surfaces/Header/Header";
+import Footer from "ui/components/surfaces/Footer/Footer";
+import { AppContainer } from "@styles/pages/_app.style";
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -17,8 +19,11 @@ function MyApp({ Component, pageProps }) {
         <link href="/fonts/tw-icons/css/treinaweb-icons.css" rel="stylesheet" />
       </Head>
       <ThemeProvider theme={theme}>
-        <Header />
-        <Component {...pageProps} />
+        <AppContainer>
+          <Header />
+          <Component {...pageProps} />
+          <Footer />
+        </AppContainer>
       </ThemeProvider>
     </>
   );
