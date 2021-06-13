59cf7f9b5b10d0b75880fdb048f795c6690250e8
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-06-12 21:05:11 -0300

    declaracao de constantes

--- src/data/hooks/pages/useIndex.page.ts
+++ src/data/hooks/pages/useIndex.page.ts
@@ -1,7 +1,14 @@
 import { useState } from 'react';
+import { UserShortInterface } from 'data/@types/UserInterface';
 
 export default function useIndex() {
-    const [cep, setCep] = useState('');
+    const 
+        [cep, setCep] = useState(''),
+        [erro, setErro] = useState(''),
+        [buscaFeita, setBuscaFeita] = useState(false),
+        [carregando, setCarregando] = useState(false),
+        [diaristas, setDiaristas] = useState([] as UserShortInterface[]),
+        [diaristasRestantes, setDiaristasRestantes] = useState(0);
 
     return {
         cep,
